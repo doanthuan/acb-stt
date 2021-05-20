02cd0f687d17e38b8b18ef4a611b859a69d07620
thaiquan <tranhaiquan.khtn@gmail.com>
2021-05-20 14:02:31 +0900

    update config and requirements

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ env = Env()
 
 
 class Settings(BaseSettings):
-    API_STT = "http://stt.dinosoft.vn/api/v1/speechtotextapi/post-and-decode-file"
-    API_URL = "http://localhost:5000/api"
+    API_STT = env.str("STT_API", "http://stt.dinosoft.vn/api/v1/speechtotextapi/post-and-decode-file")
+    STT_API_KEY = env.str("STT_API_KEY", "api-642ce45e-d48c-4811-8cae-3de45027968a")
+    API_URL = env.str("BACKEND_URL", "http://localhost:5000/api")
     UPLOAD_DIR = env.str("UPLOAD_DIR", "upload")
 
 
